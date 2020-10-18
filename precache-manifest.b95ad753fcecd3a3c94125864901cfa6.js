acd305ae42e0746443150e76ee64fd16009316a1
Victor Gil <victoredin94@hotmail.com>
2020-10-18 15:00:23 -0500

    Updates

---
+++ precache-manifest.b95ad753fcecd3a3c94125864901cfa6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b43c7eb9a4992f85fe7338b1e88320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33abeab7f8f5ac60847f",
+    "url": "/static/css/2.c0a2b1fa.chunk.css"
+  },
+  {
+    "revision": "76cad093a3040671e95e",
+    "url": "/static/css/main.a65c6d08.chunk.css"
+  },
+  {
+    "revision": "33abeab7f8f5ac60847f",
+    "url": "/static/js/2.cb7f7891.chunk.js"
+  },
+  {
+    "revision": "087eab1eba43ac37d9777ca4245630c6",
+    "url": "/static/js/2.cb7f7891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76cad093a3040671e95e",
+    "url": "/static/js/main.936058f6.chunk.js"
+  },
+  {
+    "revision": "b9a148b0fe3138badff8",
+    "url": "/static/js/runtime-main.b5a2c4ac.js"
+  },
+  {
+    "revision": "23568048f4741bdbf7f139ff919a2694",
+    "url": "/static/media/shopping-cart.23568048.svg"
+  }
+]);
